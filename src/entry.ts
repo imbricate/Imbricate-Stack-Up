321b32448725cd0b9638af4b655ee1f8431215f3
wmxpy <wm@mengw.io>
2024-11-12 15:38:16 -0600

    Add body-parser dependency and implement database creation endpoint in StackUp server

--- src/entry.ts
+++ src/entry.ts
@@ -3,7 +3,8 @@
  * @description Entry
  */
 
-import { IImbricateOrigin } from "@imbricate/core";
+import { IImbricateDatabase, IImbricateOrigin } from "@imbricate/core";
+import { json } from "body-parser";
 import express from "express";
 import { StackUpConfig } from "./definition";
 import { loadOriginsFromConfig } from "./util/load";
@@ -13,6 +14,7 @@ export const createStackUpServer = async (config: StackUpConfig): Promise<void> 
     const originMap: Map<string, IImbricateOrigin> = await loadOriginsFromConfig(config);
 
     const application = express();
+    application.use(json());
 
     application.get("/:origin/database/list", async (req, res) => {
 
@@ -26,7 +28,37 @@ export const createStackUpServer = async (config: StackUpConfig): Promise<void> 
             return;
         }
 
-        res.send(await origin.getDatabaseManager().getDatabases());
+        const databases: IImbricateDatabase[] = await origin.getDatabaseManager().getDatabases();
+
+        res.send(databases.map((database: IImbricateDatabase) => {
+            return {
+                databaseUniqueIdentifier: database.uniqueIdentifier,
+                databaseName: database.databaseName,
+            };
+        }));
+    });
+
+    application.post("/:origin/database/create", async (req, res) => {
+
+        const originUniqueIdentifier: string = req.params.origin;
+        const body: any = req.body;
+
+        const origin: IImbricateOrigin | null =
+            originMap.get(originUniqueIdentifier) ?? null;
+
+        if (!origin) {
+            res.status(404).send("Origin Not Found");
+            return;
+        }
+
+        const database: IImbricateDatabase = await origin.getDatabaseManager().createDatabase(
+            body.databaseName,
+            body.schema,
+        );
+
+        res.send({
+            databaseUniqueIdentifier: database.uniqueIdentifier,
+        });
     });
 
     application.listen(3000, () => {
